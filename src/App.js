4b93bd4ba72c253c1b2bb2933d6cb8d9a2edb9d8
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-05-15 00:52:04 +0200

    toggleTodo

--- src/App.js
+++ src/App.js
@@ -11,19 +11,31 @@ function App() {
   // setTodo is the Function we call to Update the Todos.
   const todoNameRef = useRef()
 
+
   //  Getting todos.
   useEffect(() => {
     const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
     if (storedTodos) setTodos(storedTodos)
   }, [])
 
-  //  Storing todos
+
+  //  Storing todos.
   useEffect(() => {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
   }, [todos])
   //  takes in another function as a parameter that must do things
   //  evertime something changes, I want to call the very first function
   //  evertime the todos changes I want to save my todos
+
+
+  //  Toggle todos from complete to incomplete and vice-versa.
+  function toggleTodo(id) {
+    const newTodos = [...todos] //  Copy of todosList, so the todoList does not get modified.
+    const todo = newTodos.find(todo => todo.id == id);
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+}
+
 
   function handleAddTodo(e) {
     const name = todoNameRef.current.value
@@ -36,7 +48,7 @@ function App() {
 
   return (
     <>
-      <TodoList todos={todos}/>
+      <TodoList todos={todos} toggleTodo={toggleTodo}/>
       <input ref={todoNameRef} type="text" />
       <button onClick={handleAddTodo}>Add Todo</button>
       <button>Clear Completed</button>
