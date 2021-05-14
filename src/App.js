8c96e2a82e45939495d1bf88edeb7092d335dae7
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-05-14 23:05:55 +0200

    add useRef hook
    to reference elements inside my HTML eg. input

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React, {useState} from 'react';  //import hooks with {}...to JavaScript Wolrd.
+import React, {useState, useRef} from 'react';  //import hooks with {}...to JavaScript Wolrd.
 import TodoList from './TodoList'
 
 function App() {
   const [todos, setTodo] = useState([])
   //  Call the function pass in Default state is empty array to store all the todos.
-  //  todos is all the todos inside of the Todo State
+  //  todos is all the todos inside of the Todo State.
   // setTodo is the Function we call to Update the Todos.
 
+  const todoNameRef = useRef()
+
   function handleAddTodo(e) {
-    const 
+    const name = todoNameRef.current.value
+    if (name === "") return  // Don't want to add empty todos.
+    console.log(name)
+    todoNameRef.current.value = null  // Clear input after adding todo.
   }
 
   return (
     <>
       <TodoList todos={todos}/>
-      <input type="text" />
+      <input ref={todoNameRef} type="text" />
       <button onClick={handleAddTodo}>Add Todo</button>
       <button>Clear Completed</button>
       <div>o left to do.</div>
