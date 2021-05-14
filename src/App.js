de7c09afd84b81afd9de0fdcb498b14d05ea4e18
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-05-14 22:20:46 +0200

    store all todos in a state with useState
    useState returns an Array.
    We destructure the Array with a prop and a fuction to update the state of the useState.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';  //import hooks with {}...to JavaScript Wolrd.
 import TodoList from './TodoList'
 
 function App() {
+  const [todos, setTodo] = useState(["Todo 1", "Todo 2"])
+  //  Call the function pass in Default state is empty array to store all the todos.
+  //  todos is all the todos inside of the Todo State
+  // setTodo is the Function we call to Update the Todos.
   return (
     <>
-      <TodoList />
+      <TodoList todos={todos}/>
       <input type="text" />
       <button>Add Todo</button>
       <button>Clear Completed</button>
